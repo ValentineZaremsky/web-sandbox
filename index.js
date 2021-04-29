2501f0dfb487ecb5e2d1b8b8ca951302465525e6
Valentine Zaremsky <zaremsky@gmail.com>
2021-04-29 18:10:32 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+// alert("Hello, I'm JavaScript!");
+
+
+// let name = prompt("What is your name?", '');
+
+// let result = confirm(`Your name is ${name}, are you sure?`);
+
+// alert(result);
+
+
+// let num = prompt("Введи число", 0);
+
+// if ( num > 0 ) {
+//   alert( '1' );
+// } else if ( num < 0 ) {
+//   alert( '-1' );
+// } else {
+//   alert('0')
+// }
+
+
+// let result = (a + b < 4) ? 'Мало' : 'Много';
+
+
+// let message = (login == 'Сотрудник') ? 'Привет'
+//   : (login == 'Директор')  ? 'Здравствуйте'
+//   : (login == '') ? 'Нет логина'
+//   : '';
