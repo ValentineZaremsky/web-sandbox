707ab6b3afd7f8092e11d39af3af7413dc1e7835
Valentine Zaremsky <zaremsky@gmail.com>
2021-05-19 21:10:42 +0300

    add links to authors

---
+++ get-data-by-get.js
@@ -0,0 +1,38 @@
+const ul = document.getElementById('authors');
+
+const url = 'https://randomuser.me/api/?results=16';
+
+fetch(url)
+.then((resp) => resp.json())
+.then(function(data) {
+  let authors = Array.from(data.results);
+
+  return authors.map(function(author) {
+    let li = createNode('li');
+    let a = createNode('a');
+    let img = createNode('img');
+    let span = createNode('span');
+
+    a.href = `mailto:${author.email}`
+    img.src = author.picture.large;
+    span.innerHTML = `${author.name.first} ${author.name.last}`;
+
+    append(a, img);
+    append(a, span);
+    append(li, a);
+    append(ul, li);
+  })
+})
+.catch(function(error) {
+  console.log(error);
+});
+
+function createNode(element) {
+  return document.createElement(element);
+}
+
+function append(parent, el) {
+  if (parent) {
+    return parent.append(el);
+  }
+}
--- get-data-by-post.js
+++ get-data-by-post.js
@@ -1,17 +1,31 @@
-const url = 'https://randomuser.me/api/?results=20';
 const ul = document.getElementById('authors');
 
-fetch(url)
+const url = 'https://randomuser.me/api';
+
+let data = {
+  name: 'Sara'
+}
+
+var request = new Request(url, {
+    method: 'POST',
+    body: data,
+    headers: new Headers()
+});
+
+console.log('==>', request);
+
+fetch(request)
 .then((resp) => resp.json())
 .then(function(data) {
   let authors = Array.from(data.results);
+
 
   return authors.map(function(author) {
     let li = createNode('li');
     let img = createNode('img');
     let span = createNode('span');
 
-    img.src = author.picture.medium;
+    img.src = author.picture.large;
     span.innerHTML = `${author.name.first} ${author.name.last}`;
 
     append(li, img);
@@ -29,6 +43,6 @@ function createNode(element) {
 
 function append(parent, el) {
   if (parent) {
-    return parent.appendChild(el);
+    return parent.append(el);
   }
 }
